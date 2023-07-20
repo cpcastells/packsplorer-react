e9974348cdfce5c94326b102aea3c10d50e8c43e
Carles <cpcastells@gmail.com>
2023-07-20 15:24:36 +0200

    Create Footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import FooterStyled from "./FooterStyled";
+
+const Footer = (): React.ReactElement => {
+  return (
+    <FooterStyled className="stats">
+      <em>Start adding some items to your list 🚀</em>
+    </FooterStyled>
+  );
+};
+
+export default Footer;
