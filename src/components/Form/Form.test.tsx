404e4c51bccafb170955a3909e35cc958d76c86c
Carles <cpcastells@gmail.com>
2023-07-20 13:37:18 +0200

    Create test for Form component

---
+++ src/components/Form/Form.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import Form from "./Form";
+
+describe("Given a Form component", () => {
+  describe("When it is rendered", () => {
+    test("Then it should show a button with the text 'add'", () => {
+      const expectedText = "add";
+
+      render(<Form />);
+
+      const button = screen.getByRole("button", { name: expectedText });
+
+      expect(button).toBeInTheDocument();
+    });
+  });
+});
