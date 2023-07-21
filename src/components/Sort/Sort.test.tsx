935eb47b1557af5fa65fe7f520a0465028ff81d6
Carles <cpcastells@gmail.com>
2023-07-21 20:23:28 +0200

    Add use-case to test- clicking on clear button

--- src/components/Sort/Sort.test.tsx
+++ src/components/Sort/Sort.test.tsx
@@ -2,20 +2,48 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { vi } from "vitest";
 import Sort from "./Sort";
+import { itemMocks } from "../../mocks/mocks";
 
 describe("Given a Sort component", () => {
+  const onSort = vi.fn();
+  const onClear = vi.fn();
+
   describe("When it is rendered", () => {
     test("Then it should show a select with the text 'sort by input order'", async () => {
       const expectedText = "Sort by input order";
-      const onSort = vi.fn();
 
-      render(<Sort onSort={onSort} sortBy="input" />);
+      render(
+        <Sort
+          onSort={onSort}
+          onClear={onClear}
+          items={itemMocks}
+          sortBy="input"
+        />,
+      );
 
       const select = screen.getByRole("combobox");
       await userEvent.selectOptions(select, ["input"]);
       const option = screen.getByText(expectedText);
 
       expect(option).toBeInTheDocument();
+    });
+  });
+
+  describe("When the 'clear list' button is clicked", () => {
+    test("Then it should call the handleOnClear function", async () => {
+      render(
+        <Sort
+          onSort={onSort}
+          onClear={onClear}
+          items={itemMocks}
+          sortBy="input"
+        />,
+      );
+
+      const button = screen.getByRole("button", { name: "clear list" });
+      await userEvent.click(button);
+
+      expect(onClear).toHaveBeenCalled();
     });
   });
 });
