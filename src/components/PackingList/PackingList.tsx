a53d0a4d2ae06e19bac4964ffd3ff480ca421957
Carles <cpcastells@gmail.com>
2023-07-21 17:18:04 +0200

    Pass onDeleteItem by props to PackingList component

--- src/components/PackingList/PackingList.tsx
+++ src/components/PackingList/PackingList.tsx
@@ -6,18 +6,25 @@ import { item } from "../../types";
 interface PackingListProps {
   items: item[];
   onToggleItem: (id: string) => void;
+  onDeleteItem: (id: string) => void;
 }
 
 const PackingList = ({
   items,
   onToggleItem,
+  onDeleteItem,
 }: PackingListProps): React.ReactElement => {
   return (
     <PackingListStyled>
       <h3>What's in your bag?</h3>
       <ul className="items-list">
         {items.map((item) => (
-          <Item item={item} key={item.id} onToggleItem={onToggleItem} />
+          <Item
+            item={item}
+            key={item.id}
+            onToggleItem={onToggleItem}
+            onDeleteItem={onDeleteItem}
+          />
         ))}
       </ul>
     </PackingListStyled>
