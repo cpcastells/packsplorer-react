3dbc9d8f506dca0ab6a30f317df007ed1888a3c9
Carles <cpcastells@gmail.com>
2023-07-21 21:31:03 +0200

    Add media queries to Header

--- src/components/Header/HeaderStyled.ts
+++ src/components/Header/HeaderStyled.ts
@@ -8,7 +8,15 @@ const HeaderStyled = styled.header`
     font-family: "Lobster";
     font-size: 7rem;
     letter-spacing: -5px;
-    padding: 2.4rem 0;
+    padding: 2.4rem 2rem;
+
+    @media (max-width: 505px) {
+      font-size: 5.8rem;
+    }
+
+    @media (max-width: 410px) {
+      font-size: 5.3rem;
+    }
   }
 `;
 
