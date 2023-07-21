46c45dc098c090d3b0fe83c6bceef1924b0a9a71
Carles <cpcastells@gmail.com>
2023-07-21 16:23:17 +0200

    Update tests with onToggle props

--- src/components/Item/Item.test.tsx
+++ src/components/Item/Item.test.tsx
@@ -1,13 +1,15 @@
 import { render, screen } from "@testing-library/react";
 import Item from "./Item";
 import { itemMock } from "../../mocks/mocks";
+import { vi } from "vitest";
 
 describe("Given a Item component", () => {
   describe("When it is rendered", () => {
     test("Then it should show a button with the text '❌'", () => {
       const expectedText = "❌";
+      const toggleItem = vi.fn();
 
-      render(<Item item={itemMock} />);
+      render(<Item item={itemMock} onToggleItem={() => toggleItem} />);
 
       const button = screen.getByRole("button", { name: expectedText });
 
